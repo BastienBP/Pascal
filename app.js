908d2538501f00be9c9195b616ec64ab3f953474
bbr <bbr@affini-tech.com>
2017-05-16 15:56:39 +0200

    feat: add 2 files

---
+++ app.js
@@ -0,0 +1,39 @@
+var AWS = require('aws-sdk');
+var express = require('express'), http = require('http');
+var app = express();
+
+
+AWS.config.update({
+	region: process.env.DYNAMO_REGION,
+	endpoint: process.env.DYNAMO_ENDPOINT
+});
+
+app.get('/', function (req, res) {
+	res.send('Wow');
+});
+
+app.get('/students', function (req, res) {
+	
+
+});
+
+app.get('/user/:userId', function(req, res) {
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	var params = {
+		TableName: "users",
+		Key: {
+			"userId"; req.params["userId"];
+		}
+	}
+	docClient.get(params, function(err,data) {
+		if(err) {
+			res.send("Error while reading: " + JSON.stringify(err, nulln2));
+		} else {
+			res.send("Get item: " + JSON.stringify(data, null, 2));
+		}
+	});
+})
+
+app.listen(process.env.PORT, function () {
+	console.log("server successfully started!");
+});
